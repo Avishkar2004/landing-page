e78eeb1b162909bfecf22f26ceddf9fe16d886cc
Avishkar2004 <avishkarkakde2004@gmail.com>
2023-09-26 14:50:53 +0530

    first commit

--- src/components/Brands.js
+++ src/components/Brands.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React from "react";
+import { overview } from "../data";
 
 const Brands = () => {
-  return <div>Brands</div>;
+  //destructure data
+  const { brands, productImg } = overview;
+  return (
+    <section className="py-9">
+      <div className="container mx-auto flex flex-col lg:flex-row items-center justify-between space-y-6">
+        {/* brands */}
+        {brands.map((item, index) => {
+          //destructure item
+          const { delay, image } = item;
+          return (
+            <div key={index} data-aos="fade-up" data-aos-delay={delay}>
+              {/* brand img */}
+              <img src={image} alt="" />
+            </div>
+          )
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Brands;
--- src/components/Feature2.js
+++ src/components/Feature2.js
@@ -1,7 +1,41 @@
-import React from 'react';
-
+import React from "react";
+import { features } from "../data";
 const Feature2 = () => {
-  return <div>Feature2</div>;
+  //destructure data
+  const { feature2 } = features;
+  //destructure feature2
+  const { pretitle, btnIcon, btnLink, image, subtitle, title } = feature2;
+
+  return (
+    <section className="section">
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row lg:items-center lg:gap-x-[30px]">
+          {/* img */}
+          <div
+            className="flex-1 order-2 lg:order-1"
+            data-aos="fade-right"
+            data-aos-offset="300"
+          >
+            <img src={image} alt="" />
+          </div>
+
+          {/* text */}
+          <div
+            className="flex-1 order-1 lg:order-2"
+            data-aos="fade-left"
+            data-aos-offset="400"
+          >
+            <div className="pretitle">{pretitle}</div>
+            <h2 className="title">{title}</h2>
+            <p className="lead">{subtitle}</p>
+            <button className="btn-link flex items-center gap-x-3 hover:gap-x-5 transition-all ">
+              {btnLink} <img src={btnIcon} alt="" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Feature2;
